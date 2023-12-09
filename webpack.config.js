01c8e5725fa60a1bdf90672fd89aecb982441775
csm <csm009177@gmail.com>
2023-12-09 16:18:52 +0900

    npm run build:prod with conponent.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const isProduction = process.env.NODE_ENV == 'production';
 
 
 const config = {
-    entry: './src/index.js',
+    entry: './src/component.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
     },
